b08a10c6cf1922a042fc60078387e3125f2fe974
maestro92 <marty92@gmail.com>
2018-10-23 20:20:06 -0400

    day 101

---
+++ Notes/day101 - The Inverse and the Transpose.cpp
@@ -0,0 +1,50 @@
+Handmade Hero Day 101 - The Inverse and the Transpose
+
+Summary:
+pretty much a lecture on linear algebra
+
+hinted that he will explain the next episode why normals are tranfromed by (A ^ -1) ^ T,
+Inverse Transform of the matrix, not A
+
+Keyword:
+linear algebra, normal matrix
+
+
+
+5:08
+if we do non-uniform scaling, where the x and y axis will not be scaled evenly
+the normal is will not scaled up properly
+
+6:59
+shows a graphical representation of why scaling normal with the same scale matrix to the surface 
+is incorrect. Essentially, the normals have to go through a different transformation
+
+
+12:14
+so apparently not all vectors are the same,
+
+for vertices, we usually write them as column vectors 
+
+				| px |
+				|	 |
+				| py |
+				|	 |
+				| pz |
+
+but for normal vectors, we write them as row vectors 
+
+				| nx ny nz |
+
+so they are not treated equally in computer graphics transformations
+
+
+15:02
+introduced matrix transformations
+
+and spent a bunch of time talking about linear algebra
+
+
+1:18:16
+hinted that he will explain the next episode why normals are tranfromed by (A ^ -1) ^ T
+
+	Inverse Transform of the matrix, not A
